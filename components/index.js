c0d8d76a3e34dc5dac9a6f665c985ed940e72112
Adrian Lopez <adrian@imagendigital.com>
2020-01-22 18:45:37 -0600

    set custom event for demo

--- components/index.js
+++ components/index.js
@@ -1,6 +1,25 @@
 import React, {Component} from 'react';
 import ReactDOM from  'react-dom';
 
+
+var CustomBuild = new Event('build');
+
+document.addEventListener("DOMContentLoaded", function(event) {
+ 	document.dispatchEvent(CustomBuild);
+});
+
+
+
+// Escucha para el evento.
+document.addEventListener('build', function (e) {
+  console.log("here my custom build ")
+
+}, false);
+
+
+
+
+
 class Root extends Component {
 	render(){
 		return (
