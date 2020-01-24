e0d34583316c9b247e87df0986d6d9a5a50d3ac1
Adrian Lopez <adrian@imagendigital.com>
2020-01-24 13:11:56 -0600

    Portafolios|| better chats

--- components/index.js
+++ components/index.js
@@ -1,11 +1,15 @@
 import React, { Component, useState } from 'react';
 import ReactDOM from 'react-dom';
-import MessageChannel from './messageChat'
+import { DemoMessage , MessageChannel  } from './messageChat';
+
+
+
 
 class Root extends Component {
     constructor(props) {
         super(props);
         this.chats = [];
+        this.limit = 1;
         this.state = {
             chats: []
         }
@@ -13,25 +17,36 @@ class Root extends Component {
 
     componentDidMount() {
         this.OneChatDemo();
+        let OneChat = this.OneChatDemo.bind(this);
+        let self = this;
+        window.addEventListener("chatDemo", function(e) {
+            self.limit++;
+            OneChat();
+        });
+
     }
 
     OneChatDemo() {
-        if (this.chats.length <= 10) {
-        	let status = ((  this.chats.length % 2) == 0) ? 'in' : 'out';
+        if (this.chats.length <= this.limit) {
+            let status = ((this.chats.length % 2) == 0) ? 'in' : 'out';
             let MoreChat = this.OneChatDemo.bind(this);
-            MessageChannel( status, MoreChat ).then((demoChat)=>{
-            	this.chats.push( demoChat );
-            	this.setState({ chats: this.chats });
-
+            DemoMessage(status, MoreChat).then((demoChat) => {
+                this.chats.push(demoChat);
+                this.setState({ chats: this.chats });
             });
-            
         }
+    }
+
+
+    componentDidUpdate(prevProps, prevState) {
+          var messageBody = document.querySelector('#chat-demo-beacon')
+    	.scrollTo(0, 1000);
     }
 
     render() {
         return (
             <ul id='chat-demo-beacon' class="chat-list chat-box-ul" >
-            	{ this.state.chats }	            
+            	{ this.state.chats }            		            
             </ul>
         );
     }
--- components/messageChat.js
+++ components/messageChat.js
@@ -1,33 +1,30 @@
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { Container, Button, Alert } from 'react-bootstrap';
 
 let imgIn = "https://avataaars.io/?avatarStyle=Circle&topType=Hat&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light"
 let imgOut = "https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light";
-    
-function GetBaccon(){
-  return fetch('https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1').then((response)=>{
-     return response.json();
-  });
+
+function GetBaccon() {
+    return fetch('https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1').then((response) => {
+        return response.json();
+    });
 }
 
 function scroll() {
-  
-  document.querySelector("#chat-demo-beacon")
-  .scrollIntoView(
-    {
-      behavior: "smooth", 
-      block: "end",
-      inline: "nearest"
-   });
- }
 
-let  MessageChannel = ({ status = 'in', demo, message  })=> {
+    var messageBody = document.querySelector('#chat-demo-beacon')
+    .scrollTo(0, 1000);
+    //messageBody.scrollTop = messageBody.scrollHeight;
+
+}
+
+let MessageChannel = ({ status = 'in', demo, message }) => {
     const [showButton, setShowButton] = useState(true);
     const [showMessage, setShowMessage] = useState(false);
     let imgAvatar = (status == 'in') ? imgIn : imgOut;
     let nameUser = (status == 'in') ? 'Jimmy Williams' : 'Jenny anna';
-    let position = (status == 'in') ? 'float-left' : 'float-right'; 	   
+    let position = (status == 'in') ? 'float-left' : 'float-right';   
     setTimeout(() => {
         setShowMessage(true);
     }, 1500);
@@ -38,7 +35,7 @@ let  MessageChannel = ({ status = 'in', demo, message  })=> {
           <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
           <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
           <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
- 		</div>
+     </div>
       )}
       <CSSTransition
         in={showMessage}
@@ -47,38 +44,38 @@ let  MessageChannel = ({ status = 'in', demo, message  })=> {
         unmountOnExit
         onEnter={() => setShowButton(false)}
         onEntered ={()=>{
-        	demo();
+          demo();
           scroll();
         }}
         onExited={() =>{
-        	setShowButton(true); 
+          setShowButton(true); 
         }}
       >
         <li className={ status }  >
         <div  onClick={ ()=>{ setShowMessage( false ); }} >                  
-	            <div class="chat-img">	            
-	                <img alt="Avtar" src={ imgAvatar } />
-	             </div>
-	                  
-	            <div className="chat-body">
-	                <div className="chat-message">
-	                    <h5>{nameUser}</h5>
-	                    <p>{message}</p>
-	                </div>
-	            </div>
-	            </div>
-	         </li>        
+              <div class="chat-img">              
+                  <img alt="Avtar" src={ imgAvatar } />
+               </div>
+                    
+              <div className="chat-body">
+                  <div className="chat-message">
+                      <h5>{nameUser}</h5>
+                      <p>{message}</p>
+                  </div>
+              </div>
+              </div>
+           </li>        
       </CSSTransition>
     </Container>
     );
 }
 
 
-let DemoMessage =((status = 'in', demo)=>{
-   return GetBaccon().then(( message )=>{
-       return <MessageChannel  status ={status} demo={ demo } message={ message  } />
-   });
+let DemoMessage = ((status = 'in', demo) => {
+    return GetBaccon().then((message) => {
+        return <MessageChannel  status ={status} demo={ demo } message={ message  } />
+    });
 });
 
 
-export default DemoMessage;
+export { DemoMessage , MessageChannel  };
