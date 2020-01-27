3338c72d9ab390f0248a6ac157a35d8133d931a1
Adrian Lopez <adrian@imagendigital.com>
2020-01-27 12:50:11 -0600

    portafolio || set url

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Lord cafe' });
 });
 
+
+router.get('/chatdemo', function(req, res, next) {
+  res.render('index', { title: 'Lord cafe' });
+});
 module.exports = router;
