f4dda12d1b294bd9198769dead354eb1c657236c
Adrian Lopez <adrian@imagendigital.com>
2020-01-29 15:01:52 -0600

    portafolio || better youtube

--- components/youtube/index.js
+++ components/youtube/index.js
@@ -6,52 +6,37 @@ class Youtube extends Component {
     constructor(props) {
         super(props);
         this.thumbnails = [];
-        this.state = { videos: [] };
+        this.state = { videos: [], currentId: false, datavideo: {} };
     }
 
     componentDidMount() {
         window.addEventListener('Search', (e) => {
             this.makeList(e.detail);
-
         });
     }
 
-
     makeList(list) {
-
         let thumbnails = Array.from(list).map((data, index) => {
-            return this.thumbnail(data.snippet);
+            return this.thumbnail(data, data.snippet);
         });
         this.setState((state, props) => {
             return { videos: thumbnails };
         });
-
-
     }
 
-    messageError({ message }) {
-        return (
-            <div class="alert alert-danger" role="alert">
-                <h4 class="alert-heading">Error</h4>
-                <p>{message}</p>
-                <p class="mb-0"></p>
-            </div>
-
-        );
-    }
-
-    thumbnail({ thumbnails, title, description }) {
-        console.log( thumbnails );
+    thumbnail(data, { thumbnails, title, description }) {
         return (
             <li class="thumbnail-video">
-            <div class="thumbnail">
-                <div class="flex-video widescreen ">
-                <img src={thumbnails.medium.url } class="img-thumbnail"  alt="Responsive image" />
+            <div class="thumbnail row ">
+                <div class="flex-video widescreen col-6 ">
+                <img src={thumbnails.medium.url } class="img-thumbnail"  alt="Responsive image" onClick={()=>{              
+                     this.setState((state, props) => {                    
+                     return { currentId: data.id.videoId , datavideo : data  };
+                  });
+                }}/>
                 </div>
-              <div class="caption">
-                <h3>{ title }</h3>
-                <p>{ description }</p>
-                <p></p>
+              <div class="caption col-6 description-list ">
+                <span className={'title-video-list'}>{ title }</span>               
               </div>
             </div>
           </li>
@@ -59,11 +44,53 @@ class Youtube extends Component {
         );
     }
 
+    iframevideo(id) {
+
+        
+        return (<iframe id="video-embed" class="embed-responsive-item" src={`https://www.youtube.com/embed/${id}`}
+            ></iframe>);
+    }
+
+    componentDidUpdate(){
+        let element = document.querySelector("#searchYoutube");
+        if(element){
+            window.scrollTo(0, element.offsetTop);
+        }
+        
+    }
+
+
+    PlayerYoutube(idVideo = false, { snippet = {} }) {
+        let PlayerYoutube = (this.state.currentId) ? this.iframevideo(this.state.currentId, this.state.datavideo) : '';
+        let { title = '' } = snippet;
+        let status = (idVideo) ? 'block' : 'none';
+        return (
+            <div class="container" style={{ display: status }}>                                   
+                    <div class="embed-responsive embed-responsive-16by9">
+                        {PlayerYoutube}                       
+                    </div>
+                     <span className={'title-video'}>{snippet.title}</span>
+            </div>
+        );
+    }
+
     render() {
         return (
-            <ul class="row list-search-youtube">
-            { this.state.videos }
-            </ul>
+            <div class=" AppYoutube">
+              <div class="row">
+                   
+                    <div class=" col-12  col-md-8">
+                         {this.PlayerYoutube(this.state.currentId, this.state.datavideo)}    
+                    </div>
+                     <div class=" col-12 col-md-4">
+                         <ul class="list-search-youtube">
+                            { this.state.videos }           
+                          </ul>    
+                    </div>  
+              </div>
+            </div>
+
+
         )
     }
 }
