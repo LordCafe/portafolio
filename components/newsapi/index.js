2a9c7470405c0012ae90f3ae47eb53d0188fa988
Adrian Lopez <noknows6@gmail.com>
2020-01-30 00:23:32 -0600

    portafolio || newsApi

--- components/newsapi/index.js
+++ components/newsapi/index.js
@@ -5,52 +5,95 @@ var key = "62d5413f833d4aa7808879f311bfeff7";
 class NewsApi extends Component {
     constructor(props) {
         super(props);
-        this.state ={
-            News: []
+        this.state = {
+            News: [],
+            controlButtons: false,
         }
-
     }
 
     componentDidMount() {
-        this.allNews();
+       
     }
 
     componentDidUpdate() {
 
     }
 
-    OneNews() {
+    OneNews({ title, content, author, url, urlToImage }) {
         return (
-            <div class="card">
-                <img class="card-img-top" src="https://images.pexels.com/photos/3363331/pexels-photo-3363331.jpeg?cs=srgb&dl=light-house-on-hill-3363331.jpg&fm=jpg" alt="Card image cap"/>
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
+            <div class='col-12 col-md-4 newsapi-card'>    
+            
+                <a href={ url } >
+
+                <div class="card newsapiImage ">
+                  <img class="card-img-top " src={urlToImage} alt="Card image cap"/>
+                  <div class="card-body">
+                    <h5 class="card-title newsapi-title">{title}</h5>
+                  </div>
+                </div>
+            </a>
+
         </div>
 
         );
     }
 
-    allNews(){
+    allNews() {
+        let demo = Array.from({ length: 6 }, () => this.OneNews());
+        this.setState((state, props) => {
+            return { News: demo };
+        });
+    }
 
-          let demo =   Array.from({length: 5}, () => this.OneNews());         
-           this.setState((state, props) => {
-                return { News: demo  };
+    ControlsStatusResquest() {
+
+        this.setState((state, props) => {
+            return { controlButtons: !this.state.controlButtons };
+        })
+
+
+        fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=62d5413f833d4aa7808879f311bfeff7").then((server) => {
+            return server.json();
+
+        }).then((data) => {
+            return data.articles.map((data, index) => {
+                return this.OneNews( data  );
+            })
+
+        }).then(( news )=>{
+
+            this.setState((state, props) => {
+                    return { News: news , controlButtons: !this.state.controlButtons   };
             });
+
+        });
+
     }
 
 
-
     render() {
-
+        let Loading = (this.state.controlButtons) ? 'block' : 'none';
+        let Buttons = (!this.state.controlButtons) ? 'block' : 'none';
+        let controsl = this.ControlsStatusResquest.bind(this);
         return (
             <div class="NewsApi">
-            <div className={'row'}>
-                {this.state.News}
-            </div>
-             <a class="btn btn-primary" target="_blanck" href="https://newsapi.org/" role="button">powered by NewsAPI.org</a>
+            <div class=" NewsApi-buttons">
+                <a class="btn btn-primary" target="_blanck" href="https://newsapi.org/" role="button">powered by NewsAPI.org</a>
+                <button type="button" 
+                class="btn btn-warning"
+                style={{ display:  Buttons }} 
+                onClick={controsl}
+                >Solicitar noticías</button>
+
+
+                <div class="spinner-grow" style={{width: "3rem", height: "3rem", display: Loading }} role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </div>   
+                <div className={'row'}>
+                    {this.state.News}
+                </div>
+             
             </div>
         )
     }
